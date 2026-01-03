9841366f6f5c043e9447cfdf9ac9b4fa5809e8b2
github-action-benchmark <github@users.noreply.github.com>
2026-01-03 09:47:21 +0000

    add ChampSim 64-CPU Scaling Benchmark (go) benchmark result for 8865c9ea92308116d682bc0d129c01aeecf4dc36

---
+++ dev/bench/data.js
@@ -0,0 +1,194 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767433641648,
+  "repoUrl": "https://github.com/Readm/flow_sim",
+  "entries": {
+    "ChampSim 64-CPU Scaling Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "assasaki@163.com",
+            "name": "readm",
+            "username": "Readm"
+          },
+          "committer": {
+            "email": "assasaki@163.com",
+            "name": "readm",
+            "username": "Readm"
+          },
+          "distinct": true,
+          "id": "8865c9ea92308116d682bc0d129c01aeecf4dc36",
+          "message": "ci: add write permissions for contents and pull requests to CI workflow",
+          "timestamp": "2026-01-03T17:46:52+08:00",
+          "tree_id": "a427be3221d6f9786dfaea8c5483b4a3761d9af9",
+          "url": "https://github.com/Readm/flow_sim/commit/8865c9ea92308116d682bc0d129c01aeecf4dc36"
+        },
+        "date": 1767433640713,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1",
+            "value": 306004472,
+            "unit": "ns/op\t 199950806 actual_cycles/op\t       100.0 efficiency_pct\t        64.00 sim_cpus\t         1.000 speedup\t       136.0 total_nodes\t67619686 B/op\t  524037 allocs/op",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - ns/op",
+            "value": 306004472,
+            "unit": "ns/op",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - actual_cycles/op",
+            "value": 199950806,
+            "unit": "actual_cycles/op",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - efficiency_pct",
+            "value": 100,
+            "unit": "efficiency_pct",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - sim_cpus",
+            "value": 64,
+            "unit": "sim_cpus",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - speedup",
+            "value": 1,
+            "unit": "speedup",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - total_nodes",
+            "value": 136,
+            "unit": "total_nodes",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - B/op",
+            "value": 67619686,
+            "unit": "B/op",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1 - allocs/op",
+            "value": 524037,
+            "unit": "allocs/op",
+            "extra": "4 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2",
+            "value": 185028876,
+            "unit": "ns/op\t  98069295 actual_cycles/op\t       101.9 efficiency_pct\t        64.00 sim_cpus\t         2.039 speedup\t       136.0 total_nodes\t67123974 B/op\t  519715 allocs/op",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - ns/op",
+            "value": 185028876,
+            "unit": "ns/op",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - actual_cycles/op",
+            "value": 98069295,
+            "unit": "actual_cycles/op",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - efficiency_pct",
+            "value": 101.9,
+            "unit": "efficiency_pct",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - sim_cpus",
+            "value": 64,
+            "unit": "sim_cpus",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - speedup",
+            "value": 2.039,
+            "unit": "speedup",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - total_nodes",
+            "value": 136,
+            "unit": "total_nodes",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - B/op",
+            "value": 67123974,
+            "unit": "B/op",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2 - allocs/op",
+            "value": 519715,
+            "unit": "allocs/op",
+            "extra": "6 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4",
+            "value": 168850240,
+            "unit": "ns/op\t  81171755 actual_cycles/op\t        61.58 efficiency_pct\t        64.00 sim_cpus\t         2.463 speedup\t       136.0 total_nodes\t66299820 B/op\t  509803 allocs/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - ns/op",
+            "value": 168850240,
+            "unit": "ns/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - actual_cycles/op",
+            "value": 81171755,
+            "unit": "actual_cycles/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - efficiency_pct",
+            "value": 61.58,
+            "unit": "efficiency_pct",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - sim_cpus",
+            "value": 64,
+            "unit": "sim_cpus",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - speedup",
+            "value": 2.463,
+            "unit": "speedup",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - total_nodes",
+            "value": 136,
+            "unit": "total_nodes",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - B/op",
+            "value": 66299820,
+            "unit": "B/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4 - allocs/op",
+            "value": 509803,
+            "unit": "allocs/op",
+            "extra": "7 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
