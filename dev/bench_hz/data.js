a11214d1c65efbac4e62fde2fe32f9b3b9be8dbb
github-action-benchmark <github@users.noreply.github.com>
2026-01-05 07:34:51 +0000

    add ChampSim System Benchmarks (customBiggerIsBetter) benchmark result for e1d41e2beedffd4944c00c8a6c6bae9dd275a4ba

---
+++ dev/bench_hz/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767598491843,
+  "repoUrl": "https://github.com/Readm/flow_sim",
+  "entries": {
+    "ChampSim System Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "assasaki@163.com",
+            "name": "readm",
+            "username": "Readm"
+          },
+          "committer": {
+            "email": "assasaki@163.com",
+            "name": "readm",
+            "username": "Readm"
+          },
+          "distinct": true,
+          "id": "e1d41e2beedffd4944c00c8a6c6bae9dd275a4ba",
+          "message": "align mertircs in benchmarks",
+          "timestamp": "2026-01-05T15:34:22+08:00",
+          "tree_id": "8dccba4afbfb98da27a06f0a1ac5a29458f722fa",
+          "url": "https://github.com/Readm/flow_sim/commit/e1d41e2beedffd4944c00c8a6c6bae9dd275a4ba"
+        },
+        "date": 1767598491407,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_1-4",
+            "value": 4657,
+            "unit": "sim_Hz"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_2-4",
+            "value": 8763,
+            "unit": "sim_Hz"
+          },
+          {
+            "name": "Benchmark_ChampSim_64CPU/Cores_4-4",
+            "value": 12164,
+            "unit": "sim_Hz"
+          }
+        ]
+      }
+    ]
+  }
+}
